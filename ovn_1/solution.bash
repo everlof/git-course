4c4a3ed4b9e95a8e84e65fdde832051a5d271b12
David Everlöf <everlof@gmail.com>
2020-10-16 10:41:09 +0200

    More work on presentation - also add ovn1

---
+++ ovn_1/solution.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git init
+
+# Skapa att-handla.txt
+
+git add att-handla.txt
+git commit -m "Lade till 'att-handla.txt'"
+
+# Skapa att-göra.txt
+
+git add att-göra.txt
+git commit -m "Lade till 'att-göra.txt'"
+
+# Redigera att-handla.txt
+
+git add att-handla.txt
+git commit -m "Köpte bröd - lade till Ost till handlelista"
+
